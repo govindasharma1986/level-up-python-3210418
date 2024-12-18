1d611318655b97bb217ea2209ed05ccba17490e0
govindasharma1986 <govindasharma1986@gmail.com>
2024-12-18 19:18:53 +0000

    prime factors

--- src/01 Find Prime Factors/factor.py
+++ src/01 Find Prime Factors/factor.py
@@ -1,12 +1,33 @@
-def get_prime_factors(number):
-    factors = []
-    divisor = 2
-    while divisor <= number:
+def is_prime(number: int) -> bool:
+    for i in range(2, int(number ** 0.5) + 1):
+        if number % i == 0:
+            return False
+    else:
+        return True
+
+prime_divisors = []
+for i in range(2, 100 + 1):
+    if is_prime(i):
+        prime_divisors.append(i)
+
+
+def prime_divisor(number: int) -> int:
+    if number < 2:
+        return 1
+
+    for divisor in prime_divisors:
         if number % divisor == 0:
-            factors.append(divisor)
-            number = number // divisor
-        else:
-            divisor += 1
+            return divisor
+    else:
+        return number
+
+
+def get_prime_factors(number: int) -> list:
+    factors = []
+    while number > 1:
+        divisor = prime_divisor(number)
+        number = number // divisor
+        factors.append(divisor)
     return factors
 
 
